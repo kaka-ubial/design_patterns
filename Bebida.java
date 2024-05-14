58ff446765e88acba2aa78b97cba87b158a12db9
kaka-ubial <karen.ubial@pucpr.edu.br>
2024-05-14 10:38:31 -0300

    Heranças item

--- Bebida.java
+++ Bebida.java
@@ -2,4 +2,36 @@ public class Bebida extends Item {
 
 	private boolean alcoolica;
 
+	public Bebida(String nomeItem, double precoItem, String descricaoItem, boolean alcoolica) {
+		super(nomeItem, precoItem, descricaoItem);
+		this.alcoolica = alcoolica;
+	}
+
+	public boolean isAlcoolica() {
+		return alcoolica;
+	}
+
+	public void setAlcoolica(boolean alcoolica) {
+		this.alcoolica = alcoolica;
+	}
+
+	@Override
+	public void adicionarItem() {
+
+	}
+
+	@Override
+	public void editarItem() {
+
+	}
+
+	@Override
+	public void excluirItem() {
+
+	}
+
+	@Override
+	public void listarItens() {
+
+	}
 }
--- Prato.java
+++ Prato.java
@@ -6,4 +6,54 @@ public class Prato extends Item {
 
 	private boolean lactose;
 
+	public Prato(String nomeItem, double precoItem, String descricaoItem, boolean vegano, boolean gluten, boolean lactose) {
+		super(nomeItem, precoItem, descricaoItem);
+		this.vegano = vegano;
+		this.gluten = gluten;
+		this.lactose = lactose;
+	}
+
+	public boolean isVegano() {
+		return vegano;
+	}
+
+	public void setVegano(boolean vegano) {
+		this.vegano = vegano;
+	}
+
+	public boolean isGluten() {
+		return gluten;
+	}
+
+	public void setGluten(boolean gluten) {
+		this.gluten = gluten;
+	}
+
+	public boolean isLactose() {
+		return lactose;
+	}
+
+	public void setLactose(boolean lactose) {
+		this.lactose = lactose;
+	}
+
+	@Override
+	public void adicionarItem() {
+
+	}
+
+	@Override
+	public void editarItem() {
+
+	}
+
+	@Override
+	public void excluirItem() {
+
+	}
+
+	@Override
+	public void listarItens() {
+
+	}
 }
