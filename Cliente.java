49714dd06cfd9d57708d83d089d056b13e2e3f20
babitasz <barbaratippa@gmail.com>
2024-05-14 10:10:01 -0300

    teste

--- Cliente.java
+++ Cliente.java
@@ -2,4 +2,33 @@ public class Cliente extends Usuario {
 
 	private String telefone;
 
+	public Cliente(String nome, String telefone) {
+		super(nome);
+		this.telefone = telefone;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	@Override
+	public void adicionarUsuario() {
+
+	}
+
+	@Override
+	public void excluirUsuario() {
+
+	}
+
+	@Override
+	public void editarUsuario() {
+
+	}
 }
+
+
--- Garcom.java
+++ Garcom.java
@@ -1,15 +1,34 @@
 public class Garcom extends Usuario{
-
+	//atributos
 	private String turno;
 
 	private int idGarcom;
-
+	//construtor
 	public Garcom(String nome, String turno, int idGarcom) {
 		super(nome);
 		this.turno = turno;
 		this.idGarcom = idGarcom;
 	}
+	//getters e setters
+	public String getTurno() {
+		return turno;
+	}
 
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
+	public int getIdGarcom() {
+		return idGarcom;
+	}
+
+	public void setIdGarcom(int idGarcom) {
+		this.idGarcom = idGarcom;
+	}
+
+
+
+	//métodos
 	public void adicionarUsuario(){
 
 	}
