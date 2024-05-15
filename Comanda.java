7b5aa6384d7a52c2421ba040c0ed98a071f9f48b
babitasz <barbaratippa@gmail.com>
2024-05-15 07:54:10 -0300

    Implementação da comanda 🐤

--- Comanda.java
+++ Comanda.java
@@ -1,26 +1,50 @@
 import java.util.ArrayList;
+
 public class Comanda {
-	//atributos
+
+	private static int proximoId = 1;
+
 	private int idComanda;
-
-	private ArrayList<Item> itens;
-
+	private ArrayList<Item> itensComanda;
 	private double precoTotal;
 
-	public void criarComanda() {
+	private boolean pago;
 
+	public Comanda() {
+		this.idComanda = proximoId++;
+		this.itensComanda = new ArrayList<>();
+		this.precoTotal = 0;
+		this.pago = false;
 	}
 
-	public void editarComanda() {
-
+	public void adicionarItem(Item item) {
+		itensComanda.add(item);
+		precoTotal += item.getPrecoItem();
 	}
 
-	public void excluirComanda() {
-
+	public void removerItem(Item item) {
+		itensComanda.remove(item);
+		precoTotal -= item.getPrecoItem();
 	}
 
-	public void exibirComandas() {
-
+	public double calcularPrecoTotal() {
+		double total = 0;
+		for (Item item : itensComanda) {
+			total += item.getPrecoItem();
+		}
+		return total;
 	}
 
+	public void exibirComanda() {
+		System.out.println("Comanda #" + idComanda);
+		for (Item item : itensComanda) {
+			System.out.println(item.getNomeItem() + ": R$" + item.getPrecoItem());
+		}
+		System.out.println("Total: R$" + precoTotal);
+	}
+
+	public void pagarComanda(){
+		pago = true;
+		System.out.println("A comanda " + idComanda + "foi paga");
+	}
 }
