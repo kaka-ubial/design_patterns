f902ad30a6c562a0fd14dc2e8ce7fb1ecb5c771d
babitasz <barbaratippa@gmail.com>
2024-05-21 09:42:23 -0300

    interface menu das mesas 🐤

---
+++ MenuMesa.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class MenuMesa {
+    private JButton adicionarMesaButton;
+    private JButton excluirMesaButton;
+    private JButton verificarReservaButton;
+    private JButton reservarMesaButton;
+    private JButton voltarButton;
+}
