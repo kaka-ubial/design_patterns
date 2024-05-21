57462c4614899f5f382915a58b6d1797446b8313
kaka-ubial <karen.ubial@pucpr.edu.br>
2024-05-21 10:29:42 -0300

    interface-menuprincipal

---
+++ InterfaceMenu.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class InterfaceMenu {
+    private JLabel menuPrincipal;
+    private JPanel interfaceMenu;
+    private JButton comandasAbertasButton;
+    private JButton menusButton;
+    private JButton reservasButton;
+    private JButton mesasButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
