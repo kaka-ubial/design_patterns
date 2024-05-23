5cae6d909caab96b8f0636252c90059bc22218c0
babitasz <barbaratippa@gmail.com>
2024-05-23 11:34:56 -0300

    mudanças bd 🐤

--- Main.java
+++ Main.java
@@ -1,5 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+
 import java.sql.SQLException;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -10,15 +9,6 @@ public class Main {
     private static int lastGarcomId = 1;
 
     public static void main(String[] args) {
-
-
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurante", "root", "PUC@1234");
-            System.out.println("Conexão estabelecida com sucesso!");
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return;
-        }
 
         new Screen();
         int idGarcom;
